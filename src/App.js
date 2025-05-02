8eae3805bb8e0ddfc1b0a0a93f317c25fa47a2f4
Edin <edin.vatrich@gmail.com>
2025-05-02 18:29:52 +0200

    Basic engine added

--- src/App.js
+++ src/App.js
@@ -6,20 +6,157 @@ const fields = ['', '', '', '', '', '', '', '', ''];
 function App() {
   const [counter, setCounter] = useState(0);
   const [newFields, setNewFields] = useState(fields);
+  const [result, setResult] = useState(false);
 
   const handleClick = (index) => {
-    if (counter == 0 || counter % 2 == 0) {
+    if (counter > 9) {
+      setResult(true);
+      return;
+    }
+    if (counter === 0 || counter % 2 === 0) {
       const updateFields = [...newFields];
-      updateFields[index] = 'X';
+      if (updateFields[index] === '') updateFields[index] = 'X';
       setNewFields(updateFields);
       setCounter(counter + 1);
+
+      //check if there are 3 in a row or a column or diagonally
+
+      if (
+        updateFields[0] === 'X' &&
+        updateFields[1] === 'X' &&
+        updateFields[2] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[3] === 'X' &&
+        updateFields[4] === 'X' &&
+        updateFields[5] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[6] === 'X' &&
+        updateFields[7] === 'X' &&
+        updateFields[8] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[0] === 'X' &&
+        updateFields[3] === 'X' &&
+        updateFields[6] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[1] === 'X' &&
+        updateFields[4] === 'X' &&
+        updateFields[7] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[2] === 'X' &&
+        updateFields[5] === 'X' &&
+        updateFields[8] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[0] === 'X' &&
+        updateFields[4] === 'X' &&
+        updateFields[8] === 'X'
+      ) {
+        alert('X won');
+      }
+
+      if (
+        updateFields[2] === 'X' &&
+        updateFields[4] === 'X' &&
+        updateFields[6] === 'X'
+      ) {
+        alert('X won');
+      }
     }
 
     if (counter % 2 !== 0) {
       const updateFields = [...newFields];
-      updateFields[index] = 'O';
+      if (updateFields[index] === '') updateFields[index] = 'O';
       setNewFields(updateFields);
       setCounter(counter + 1);
+
+      //check if there are 3 in a row or a column or diagonally
+
+      if (
+        updateFields[0] === 'O' &&
+        updateFields[1] === 'O' &&
+        updateFields[2] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[3] === 'O' &&
+        updateFields[4] === 'O' &&
+        updateFields[5] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[6] === 'O' &&
+        updateFields[7] === 'O' &&
+        updateFields[8] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[0] === 'O' &&
+        updateFields[3] === 'O' &&
+        updateFields[6] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[1] === 'O' &&
+        updateFields[4] === 'O' &&
+        updateFields[7] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[2] === 'O' &&
+        updateFields[5] === 'O' &&
+        updateFields[8] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[0] === 'O' &&
+        updateFields[4] === 'O' &&
+        updateFields[8] === 'O'
+      ) {
+        alert('O won');
+      }
+
+      if (
+        updateFields[2] === 'O' &&
+        updateFields[4] === 'O' &&
+        updateFields[6] === 'O'
+      ) {
+        alert('O won');
+      }
     }
 
     console.log(index);
@@ -38,6 +175,7 @@ function App() {
           </div>
         ))}
       </div>
+      {setResult && <div className="result">Game Over</div>}
     </div>
   );
 }
