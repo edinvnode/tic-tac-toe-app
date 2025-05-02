a04bff61373ff45a41c3a1684526ccf3bc539a1b
Edin <edin.vatrich@gmail.com>
2025-05-02 17:53:20 +0200

    Created basic game engine

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Cell from './componets/Cell';
 
-const fields = ['', '', '', '', '', 'X', '', '', 'X'];
+const fields = ['', '', '', '', '', '', '', '', ''];
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [newFields, setNewFields] = useState(fields);
+
+  const handleClick = (index) => {
+    if (counter == 0 || counter % 2 == 0) {
+      const updateFields = [...newFields];
+      updateFields[index] = 'X';
+      setNewFields(updateFields);
+      setCounter(counter + 1);
+    }
+
+    if (counter % 2 !== 0) {
+      const updateFields = [...newFields];
+      updateFields[index] = 'O';
+      setNewFields(updateFields);
+      setCounter(counter + 1);
+    }
+
+    console.log(index);
+    console.log(fields[index]);
+    console.log(newFields);
+  };
 
   return (
     <div className="App">
       <h1>Tic Tac Toe App</h1>
 
       <div className="container">
-        {fields.map((field, index) => (
-          <div>
-            <Cell text={fields[index]} />
+        {newFields.map((field, index) => (
+          <div className="cell" onClick={() => handleClick(index)}>
+            <Cell text={field} />
           </div>
         ))}
       </div>
